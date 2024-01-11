c300c3e2ee2a4e5d60cdc72a92aac29318d37f7c
khachatur <khachatur.khachatryan@softconstruct.com>
2024-01-11 11:48:53 +0400

    add black formatter

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
+from typing import Annotated
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
+from users.views import router as users_router
 
 app = FastAPI()
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app.include_router(users_router)
 
 
 @app.get("/hello/{name}/")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+@app.get("/items/{item_id}/")
+def get_item_by_id(item_id: Annotated[int, Path(ge=1, lt=1_000_000)]):
+    return {
+        "item": {
+            "id": item_id,
+        },
+    }
 
 
 if __name__ == "__main__":
