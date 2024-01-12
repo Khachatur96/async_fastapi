95ce6978f8296edbcdc1f9004489f4ea6aa40a34
khachatur <khachatur.khachatryan@softconstruct.com>
2024-01-12 11:30:23 +0400

    cretae posts table

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,12 +1,7 @@
-__all__ = (
-    "Base",
-    "db_helper",
-    "DatabaseHelper",
-    "Product",
-    "User",
-)
+__all__ = ("Base", "db_helper", "DatabaseHelper", "Product", "User", "Post")
 
 from .base import Base
 from .product import Product
 from .user import User
+from .post import Post
 from .db_helper import DatabaseHelper, db_helper
