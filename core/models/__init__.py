e1b295b38cb6adeff7d19704e1987d5a1a36a61a
khachatur <khachatur.khachatryan@softconstruct.com>
2024-01-12 11:56:58 +0400

    create table profile

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,7 +1,16 @@
-__all__ = ("Base", "db_helper", "DatabaseHelper", "Product", "User", "Post")
+__all__ = (
+    "Base",
+    "db_helper",
+    "DatabaseHelper",
+    "Product",
+    "User",
+    "Post",
+    "Profile",
+)
 
 from .base import Base
 from .product import Product
 from .user import User
 from .post import Post
+from .profile import Profile
 from .db_helper import DatabaseHelper, db_helper
